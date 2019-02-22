2c4e68a0450599dafaa16956e2d6566a7b0708f3
Sulman B N <sulmanbn@ornl.gov>
2019-02-22 10:44:13 -0500

    Did some reorganization and changed fcur parameters backa to default values (1.0 and 0.0 for evergreen and deciduous)

--- scripts/update_kougarok_params.py
+++ scripts/update_kougarok_params.py
@@ -55,83 +55,102 @@ if __name__=='__main__':
         print('{ecotype:s}: {pft:s} leaf frac {leafCfrac:1.2f}, froot_leaf = {froot_leaf:1.2f}'.format(leafCfrac=leafCfrac[ecotype][pft],ecotype=ecotype,pft=pft,froot_leaf=froot_leaf))
         return froot_leaf
 
+    fcur_deciduous=0.0
+    fcur_evergreen=1.0
+
     # evergreen dwarf shrub
+    pft='arctic_evergreen_shrub_dwarf'
     froot_leaf_TT=froot_leaf('TT','dwarf shrub evergreen')
     froot_leaf_NAMC=froot_leaf('NAMC','dwarf shrub evergreen')
-    change_param('froot_leaf','arctic_evergreen_shrub_dwarf',froot_leaf_NAMC )
-    change_param('froot_long','arctic_evergreen_shrub_dwarf',1.5) # Value from Verity's data description Table 3
+    change_param('froot_leaf',pft,froot_leaf_NAMC )
+    change_param('froot_long',pft,1.5) # Value from Verity's data description Table 3
+    change_param('fcur',pft,fcur_evergreen)
 
     # SLA in Verity's data is in cm2/g. Parameter in model is in m2/g. Divide obs by 100**2 to convert units
-    change_param('slatop','arctic_evergreen_shrub_dwarf',Koug_meas_chem['LeafSLA_cm2perg'][:,'dwarf shrub evergreen'].mean()/100**2)
-    change_param('leafcn','arctic_evergreen_shrub_dwarf',obs_leafCN[:,'dwarf shrub evergreen'].mean())
-    change_param('frootcn','arctic_evergreen_shrub_dwarf',obs_frootCN)
+    change_param('slatop',pft,Koug_meas_chem['LeafSLA_cm2perg'][:,'dwarf shrub evergreen'].mean()/100**2)
+    change_param('leafcn',pft,obs_leafCN[:,'dwarf shrub evergreen'].mean())
+    change_param('frootcn',pft,obs_frootCN)
 
 
     printnote('Model divides stems into "dead" (heartwood) and live components with different C:N ratios. How to compare with measurements?')
 
     # Dwarf deciduous shrub
+    pft='arctic_deciduous_shrub_dwarf'
     froot_leaf_DSLT=froot_leaf('DSLT','dwarf shrub deciduous')
     froot_leaf_WBT=froot_leaf('WBT','dwarf shrub deciduous')
     # Setting this to total root:leaf ratio of DSLT which is mostly shrubs.
     printnote('Setting dwarf deciduous shrub root_leaf to total root:leaf ratio for DSLT, which is mostly shrubs')
-    change_param('froot_leaf','arctic_deciduous_shrub_dwarf',meas_root_C['DSLT']/meas_leaf_C['DSLT'].sum()  )
+    change_param('froot_leaf',pft,meas_root_C['DSLT']/meas_leaf_C['DSLT'].sum()  )
 
-    change_param('slatop','arctic_deciduous_shrub_dwarf',Koug_meas_chem['LeafSLA_cm2perg'][:,'dwarf shrub deciduous'].mean()/100**2)
-    change_param('leafcn','arctic_deciduous_shrub_dwarf',obs_leafCN[:,'dwarf shrub deciduous'].mean())
-    change_param('frootcn','arctic_deciduous_shrub_dwarf',obs_frootCN)
+    change_param('slatop',pft,Koug_meas_chem['LeafSLA_cm2perg'][:,'dwarf shrub deciduous'].mean()/100**2)
+    change_param('leafcn',pft,obs_leafCN[:,'dwarf shrub deciduous'].mean())
+    change_param('frootcn',pft,obs_frootCN)
+    change_param('fcur',pft,fcur_deciduous)
 
 
     # Low deciduous shrub
+    pft='arctic_deciduous_shrub_low'
     change_param('froot_leaf','arctic_deciduous_shrub_low',meas_root_C['DSLT']/meas_leaf_C['DSLT'].sum()  )
 
-    change_param('slatop','arctic_deciduous_shrub_low',Koug_meas_chem['LeafSLA_cm2perg'][:,'low shrub deciduous'].mean()/100**2)
-    change_param('leafcn','arctic_deciduous_shrub_low',obs_leafCN[:,'low shrub deciduous'].mean())
-    change_param('frootcn','arctic_deciduous_shrub_low',obs_frootCN)
+    change_param('slatop',pft,Koug_meas_chem['LeafSLA_cm2perg'][:,'low shrub deciduous'].mean()/100**2)
+    change_param('leafcn',pft,obs_leafCN[:,'low shrub deciduous'].mean())
+    change_param('frootcn',pft,obs_frootCN)
+    change_param('fcur',pft,fcur_deciduous)
 
 
     # Tall non-alder shrub
+    pft='arctic_deciduous_shrub_tall'
     # Set this to total root_leaf ratio of all shrubs in WBT
     printnote('Using total root:leaf ratio of all shrubs in WBT for tall non-alder shrubs')
     leaf_shrubs_WBT = (meas_leaf_C['WBT'].sum()-meas_leaf_C['WBT']['graminoid'])
     froot_shrubs_WBT = meas_root_C['WBT']*(meas_leaf_C['WBT'].sum()-meas_leaf_C['WBT']['graminoid'])/meas_leaf_C['WBT'].sum()
-    change_param('froot_leaf','arctic_deciduous_shrub_tall',froot_shrubs_WBT/leaf_shrubs_WBT)
+    change_param('froot_leaf',pft,froot_shrubs_WBT/leaf_shrubs_WBT)
 
-    change_param('slatop','arctic_deciduous_shrub_tall',Koug_meas_chem['LeafSLA_cm2perg'].loc[:,['tall shrub deciduous willow','tall shrub deciduous birch']].mean()/100**2)
+    change_param('slatop',pft,Koug_meas_chem['LeafSLA_cm2perg'].loc[:,['tall shrub deciduous willow','tall shrub deciduous birch']].mean()/100**2)
     printnote('Deciduous shrub measured C:N varies a lot, from 11 to 31. Mean is 22.')
-    change_param('leafcn','arctic_deciduous_shrub_tall',obs_leafCN.loc[:,['tall shrub deciduous birch','tall shrub deciduous willow']].mean())
-    change_param('frootcn','arctic_deciduous_shrub_tall',obs_frootCN)
+    change_param('leafcn',pft,obs_leafCN.loc[:,['tall shrub deciduous birch','tall shrub deciduous willow']].mean())
+    change_param('frootcn',pft,obs_frootCN)
+    change_param('fcur',pft,fcur_deciduous)
     
     # Alder
+    pft='arctic_deciduous_shrub_alder'
     froot_leaf_AS=froot_leaf('AS','tall shrub deciduous alder')
     froot_leaf_TTWBT=froot_leaf('TTWBT','tall shrub deciduous alder')
-    change_param('froot_leaf','arctic_deciduous_shrub_alder',froot_leaf_AS)
+    change_param('froot_leaf',pft,froot_leaf_AS)
 
-    change_param('slatop','arctic_deciduous_shrub_alder',Koug_meas_chem['LeafSLA_cm2perg'][:,'tall shrub deciduous alder'].mean()/100**2)
-    change_param('leafcn','arctic_deciduous_shrub_alder',obs_leafCN[:,'tall shrub deciduous alder'].mean())
-    change_param('frootcn','arctic_deciduous_shrub_alder',obs_frootCN)
+    change_param('slatop',pft,Koug_meas_chem['LeafSLA_cm2perg'][:,'tall shrub deciduous alder'].mean()/100**2)
+    change_param('leafcn',pft,obs_leafCN[:,'tall shrub deciduous alder'].mean())
+    change_param('frootcn',pft,obs_frootCN)
+    change_param('fcur',pft,fcur_deciduous)
 
 
     # Graminoid
+    pftwet='arctic_wet_graminoid'
+    pftdry='arctic_dry_graminoid'
     froot_leaf_TT=froot_leaf('TT','graminoid')
     froot_leaf_TTWBT=froot_leaf('TTWBT','graminoid')
     froot_leaf_WBT=froot_leaf('WBT','graminoid')
     # Use mean of TT and TTWBT, which have more graminoids and similar values
     # Use same values for wet and dry graminoids in model for now
     printnote('Using same parameter values for wet and dry graminoids')
-    change_param('froot_leaf','arctic_dry_graminoid',0.5*(froot_leaf_TT+froot_leaf_TTWBT))
-    change_param('froot_leaf','arctic_wet_graminoid',0.5*(froot_leaf_TT+froot_leaf_TTWBT))
+    change_param('froot_leaf',pftdry,0.5*(froot_leaf_TT+froot_leaf_TTWBT))
+    change_param('froot_leaf',pftwet,0.5*(froot_leaf_TT+froot_leaf_TTWBT))
 
     printnote('Graminoid SLA is much higher in WBT than other sites. What to do about that?')
-    change_param('slatop','arctic_wet_graminoid',Koug_meas_chem['LeafSLA_cm2perg'][:,'graminoid'].mean()/100**2)
-    change_param('slatop','arctic_dry_graminoid',Koug_meas_chem['LeafSLA_cm2perg'][:,'graminoid'].mean()/100**2)
-    change_param('leafcn','arctic_dry_graminoid',obs_leafCN[:,'graminoid'].mean())
-    change_param('leafcn','arctic_wet_graminoid',obs_leafCN[:,'graminoid'].mean())
-    change_param('frootcn','arctic_dry_graminoid',obs_frootCN)
-    change_param('frootcn','arctic_wet_graminoid',obs_frootCN)
+    change_param('slatop',pftwet,Koug_meas_chem['LeafSLA_cm2perg'][:,'graminoid'].mean()/100**2)
+    change_param('slatop',pftdry,Koug_meas_chem['LeafSLA_cm2perg'][:,'graminoid'].mean()/100**2)
+    change_param('leafcn',pftdry,obs_leafCN[:,'graminoid'].mean())
+    change_param('leafcn',pftwet,obs_leafCN[:,'graminoid'].mean())
+    change_param('frootcn',pftdry,obs_frootCN)
+    change_param('frootcn',pftwet,obs_frootCN)
 
     # Values from TT in Verity's data description Table 3
-    change_param('froot_long','arctic_wet_graminoid',3.13)
-    change_param('froot_long','arctic_dry_graminoid',3.13)
+    change_param('froot_long',pftwet,3.13)
+    change_param('froot_long',pftdry,3.13)
+
+    change_param('fcur',pftwet,fcur_deciduous)
+    change_param('fcur',pftdry,fcur_deciduous)
+
 
     # Forb
     # Probably not enough data to constrain roots (no site with high forb coverage). Make same as graminoids?
@@ -139,9 +158,11 @@ if __name__=='__main__':
     printnote('Not enough forb biomass at any site to estimate associated root biomass. Assume the ratio is the same as forbs?')
     printnote('No SLA measurements for forbs. Current forb value is {forbsla:1.2g}'.format(forbsla=params['slatop'].values[pft_names.index('arctic_forb')])) 
 
-    
-    change_param('leafcn','arctic_forb',obs_leafCN[:,'forb'].mean())
-    change_param('frootcn','arctic_forb',obs_frootCN)
+    pft='arctic_forb' 
+    change_param('leafcn',pft,obs_leafCN[:,'forb'].mean())
+    change_param('frootcn',pft,obs_frootCN)
+ 
+    change_param('fcur',pft,fcur_deciduous)
 
     print('Saving params file to clm_params_updated.nc')
     params.to_netcdf('clm_params_updated.nc')
